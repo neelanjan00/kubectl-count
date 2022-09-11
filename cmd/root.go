57bfd34d5c146414253a4fbc2391641f6e6a9078
Neelanjan Manna <neelanjan.manna@harness.io>
2022-09-11 19:34:40 +0530

    updated command structure
    Signed-off-by: Neelanjan Manna <neelanjan.manna@harness.io>

--- cmd/root.go
+++ cmd/root.go
@@ -6,17 +6,29 @@ import (
 	"github.com/spf13/cobra"
 )
 
-// rootCmd represents the base command when called without any subcommands
-var rootCmd = &cobra.Command{
-	Use:   "kubectl count",
-	Short: "A kubectl plugin for counting kubernetes resources.",
-	Long:  `A kubectl plugin for counting kubernetes resources, which can also be filtered using namespaces and label selectors.`,
+// stores value of the all-namespaces flag
+var allNamespaces bool
+
+// stores value of the namespace flag
+var namespace string
+
+// stores value of the selector flag
+var selector string
+
+// countCmd represents the count command
+var countCmd = &cobra.Command{
+	Use:   "count",
+	Short: "Count Kubernetes resource instances.",
+	Long:  `Count Kubernetes resource instances.`,
+	Run: func(cmd *cobra.Command, args []string) {
+		cmd.Usage()
+	},
 }
 
 // Execute adds all child commands to the root command and sets flags appropriately.
 // This is called by main.main(). It only needs to happen once to the rootCmd.
 func Execute() {
-	err := rootCmd.Execute()
+	err := countCmd.Execute()
 	if err != nil {
 		os.Exit(1)
 	}
